42d71adc2e3235e28f40d76215578f37b8c1abd7
NavinVenkatV <vnavinvenkat@gmail.com>
2025-02-16 22:43:15 +0530

    ad

---
+++ src/app/component/ui/ad.tsx
@@ -0,0 +1,7 @@
+export const Ad = ()=>{
+    return (
+        <div className="flex justify-center w-screen h-auto items-center font-light md:text-xs text-[7px] bg-[rgb(118,48,128)] text-white">
+            <i>Enjoy pingmME with free plan for 1000 checks !!</i>
+        </div>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from "./component/provider";
+import { Ad } from "./component/ui/ad";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>
+          <Ad/>
          {children}
         </Provider>
       </body>
