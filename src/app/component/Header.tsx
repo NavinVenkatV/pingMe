c1173227a48995c3f2654e1925b701ef885e2ac2
NavinVenkatV <vnavinvenkat@gmail.com>
2025-02-16 20:52:12 +0530

    color

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -42,7 +42,7 @@ export default function Header({ setIsSideOpen }: typess) {
           </div>
           <div className="hidden md:block">
             <Link href="https://github.com/NavinVenkatV/pingMe"
-              className="px-2 py-1 bg-blue-500 hover:bg-blue-700 rounded-xl text-white transition duration-300 relative z-10 flex justify-center items-center"
+              className="px-2 py-1 bg-[rgb(118,48,128)] hover:bg-[rgb(74,18,75)] rounded-xl text-white transition duration-300 relative z-10 flex justify-center items-center"
             >
               GitHub
             </Link>
