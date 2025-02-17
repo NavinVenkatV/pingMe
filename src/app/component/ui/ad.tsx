d71b9280fa1b62888c6f3d696749d96fa9bf3ab3
NavinVenkatV <vnavinvenkat@gmail.com>
2025-02-17 10:13:37 +0530

    ad_typo

--- src/app/component/ui/ad.tsx
+++ src/app/component/ui/ad.tsx
@@ -1,7 +1,7 @@
 export const Ad = ()=>{
     return (
         <div className="flex overflow-hidden justify-center  h-auto items-center font-light md:text-xs text-[7px] bg-[rgb(118,48,128)] text-white">
-            <i>Enjoy pingmME with free plan for 1000 checks !!</i>
+            <i>Enjoy pingmMe with free plan for 1000 checks !!</i>
         </div>
     )
 }
